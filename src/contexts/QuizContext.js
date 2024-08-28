78a3a14f258d26408a0f46d917fc9b44b690117e
Ahmed S.Dahy <115500490+AhmeedDahy@users.noreply.github.com>
2024-08-28 05:27:53 +0300

    Update QuizContext.js

--- src/contexts/QuizContext.js
+++ src/contexts/QuizContext.js
@@ -83,7 +83,7 @@ function QuizProvider({ children }) {
   );
 
   useEffect(function () {
-    fetch("http://localhost:9000/questions")
+    fetch("https://my-json-server.typicode.com/AhmeedDahy/React-quiz-/questions")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
